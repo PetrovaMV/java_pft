ae767e5a3dd72825c6e89ce57277f1434522d503
PetrovaMV <41550995+PetrovaMV@users.noreply.github.com>
2021-04-26 17:59:01 +0300

    Форматирование по красоте

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyProgram.java
@@ -1,8 +1,8 @@
 package ru.srqa.pft.sandbox;
 
 public class MyProgram {
-	
-	public static void main (String[] args){ 
-	System.out.println("Hello, world!");
- }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
