bb7c2adcf6f8acf560752cb5ded5c354f19e1435
PetrovaMV <41550995+PetrovaMV@users.noreply.github.com>
2021-04-28 01:52:32 +0300

    Задание 2. Потренироваться использовать функции, классы, объекты и методы.
    1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.
    
    2. Создать функцию
    public static double distance(Point p1, Point p2)
    которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
    
    3. Сделать запускаемый класс, то есть содержащий функцию
    public static void main(String[] args) {...}
    и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
    
    4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции.

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyProgram.java
@@ -3,16 +3,30 @@ package ru.srqa.pft.sandbox;
 public class MyProgram {
 
   public static void main(String[] args) {
-   hello("world");
-   hello("user");
+    hello("world");
+    hello("user");
     hello("Marina");
     Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle (4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " +  r.b + "  = " + r.area());
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + "  = " + r.area());
   }
-public static void hello(String somebody) {
-  System.out.println("Hello," + somebody + "!");
-}
-}
+
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public static class Point{
+
+    public static void main(String[] args) {
+
+      ru.srqa.pft.sandbox.Point P = new ru.srqa.pft.sandbox.Point(1, 2, 3, 4);
+      System.out.println("Расстояние между точками " + "= "  + distance(P));
+    }
+    public static double distance (ru.srqa.pft.sandbox.Point P){
+      return Math.sqrt((P.p3-P.p1)*(P.p3-P.p1)+(P.p4-P.p2)*(P.p4-P.p2));
+    }
+  }
+
+  }
